596e2ed0471c807acccc4230f541fa5e213d2b04
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2026-02-05 20:44:20 +0000

    Refactor cutscene handling for flight scenes

--- cutscene.lua
+++ cutscene.lua
@@ -1,73 +1,196 @@
+local spawnedPeds = {}
 
-RegisterNetEvent('al-cayoflight:takeoffLSA', function()
+
+function ScenesToBitwise(scenes)
+    local flags = 0
+    for _, sceneNum in ipairs(scenes) do
+        flags = flags | (1 << sceneNum)
+    end
+    return flags
+end
+
+local CayoFlightConfig = {
+    LSIA_TO_CAYO = {
+        cutscene = {{name = 'hs4_nimb_lsa_isd', playbackList = ScenesToBitwise({6,10,11}), endTime = 8000}},
+        sceneLoadCoords = vector3(-1303.55, -3160.9, 16.62),
+        destinationCoords = vector3(4432.96, -4493.7, 3.2),
+        waitTime = 8000,
+        target = {
+            ped = "s_m_y_ammucity_01",
+            coords = vector4(-998.95, -2954.95, 12.94, 119.05),
+            options = {
+                {
+                    icon = 'fas fa-plane-departure',
+                    label = 'Take off to Cayo Perico',
+                    onSelect = function()
+                        takeOffLSIA()
+                    end
+                }
+            }
+        }
+    },
+    CAYO_TO_LSIA = {
+        cutscene = {{name = 'hs4_nimb_isd_lsa', playbackList = ScenesToBitwise({3}), endTime = 8000}, {name = "hs4_lsa_land_nimb", playbackList = ScenesToBitwise({0}), endTime = 8000}},
+        sceneLoadCoords = vector3(4432.96, -4493.7, 3.2),
+        destinationCoords = vector3(-994.61, -2949.04, 13.96),
+        target = {
+            ped = "s_m_y_ammucity_01",
+            coords = vector4(4432.96, -4493.7, 3.2, 158.52),
+            options = {
+                {
+                    icon = 'fas fa-plane-departure',
+                    label = 'Take off to Los Santos International Airport',
+                    onSelect = function()
+                        takeOffCayo()
+                    end
+                }
+            }
+        }
+    }
+}
+
+function playCutsceneTravel(config)
+    local playerPed = PlayerPedId()
+    local characterModel = 'MP_1'
+    
     DoScreenFadeOut(500)
     Wait(2000)
-    DoScreenFadeIn(1000)    
-    local plyrId = PlayerPedId()
-    local thisCutsceneName = 'hs4_lsa_take_vel'
-	local charName = 'MP_1'
-    local clone = ClonePed(plyrId, false, false, true)
-
-    RequestCutscene(thisCutsceneName)
-    while not HasCutsceneLoaded() do Wait(10) end 
-    RegisterEntityForCutscene(clone, charName, 0, 0, 0)
-    SetCutsceneEntityStreamingFlags(charName, 0, 1) 
-    StartCutscene(0)
-
-	NewLoadSceneStartSphere(-1303.55, -3160.9, 16.62, 1000, 0)
-
-
-    StartCutscene(0)
-    SetEntityCoords(plyrId, vector3(4432.96, -4493.7, 3.2))
-
     
-    while not (DoesCutsceneEntityExist('MP_1', 0)) do
-        Wait(0)
+    local pedClone = ClonePed(playerPed, false, false, true)
+    if not pedClone or not DoesEntityExist(pedClone) then
+        print("Failed to clone player ped")
+        DoScreenFadeIn(500)
+        return false
+    end    
+
+    SetEntityCoords(playerPed, config.destinationCoords.x, config.destinationCoords.y, config.destinationCoords.z)
+    
+    NetworkStartSoloTutorialSession()
+
+    while not NetworkIsInTutorialSession() do
+        Wait(100)
     end
 
-    ClonePedToTarget(plyrId, clone)
+    for i, cutsceneData in ipairs(config.cutscene) do
+        local cutsceneName = cutsceneData.name
+        
+        if cutsceneData.playbackList then
+            RequestCutsceneWithPlaybackList(cutsceneName, cutsceneData.playbackList, 8)
+        else
+            RequestCutscene(cutsceneName)
+        end
+        
+        local currTime = GetGameTimer()
+        while not HasCutsceneLoaded() and GetGameTimer() - currTime < 10000 do
+            Wait(100)
+        end
 
-    Wait(12000)
-    DoScreenFadeOut(500)
-    Wait(2000)
-    StopCutsceneImmediately()
-    DoScreenFadeIn(500)    
-end) 
+        if not HasCutsceneLoaded() then
+            print("Error: Cutscene failed to load")
+            DeletePed(pedClone)
+            DoScreenFadeIn(500)
+            return false
+        end
+
+        RegisterEntityForCutscene(pedClone, characterModel, 0, 0, 0)
+        SetCutsceneEntityStreamingFlags(characterModel, 0, 1)
+
+        NewLoadSceneStartSphere(config.sceneLoadCoords.x, config.sceneLoadCoords.y, config.sceneLoadCoords.z, 1000, 0)
+        
+        Wait(500)
+        
+        StartCutscene(0)
 
 
-RegisterNetEvent('al-cayoflight:takeoffCayo', function()
-    DoScreenFadeOut(500)
-    Wait(2000)
-    DoScreenFadeIn(500) 
-    local plyrId = PlayerPedId()
-    local thisCutsceneName = 'hs4_lsa_land_vel'
-	local charName = 'MP_1'
-    local clone = ClonePed(plyrId, false, false, true)
+        local currTime = GetGameTimer()
+        while not IsCutscenePlaying() and GetGameTimer() - currTime < 5000 do
+            Wait(0)
+        end
+        DoScreenFadeIn(500)
 
-    RequestCutscene(thisCutsceneName)
-    while not HasCutsceneLoaded() do Wait(10) end 
-    RegisterEntityForCutscene(clone, charName, 0, 0, 0)
-    SetCutsceneEntityStreamingFlags(charName, 0, 1) 
-    StartCutscene(0)
+        local currTime = GetGameTimer()
+        while not DoesCutsceneEntityExist(characterModel, 0) and GetGameTimer() - currTime < 10000 do
+            Wait(100)
+        end
 
-	NewLoadSceneStartSphere(4432.96, -4493.7, 3.2, 1000, 0)
+        if DoesCutsceneEntityExist(characterModel, 0) then
+            ClonePedToTarget(playerPed, pedClone)
+        end
 
+        while not HasCutsceneFinished() do
+            Wait(0)
+        end
+
+        DoScreenFadeOut(0)
+
+
+        StopCutsceneImmediately()
+        RemoveCutscene()
+        NetworkEndTutorialSession()
+
+    end
+    
+    if DoesEntityExist(pedClone) then
+        DeletePed(pedClone)
+    end
+
+    Wait(500)
     DoScreenFadeIn(500)
-    StartCutscene(0)
-    SetEntityCoords(plyrId, vector3(-994.61, -2949.04, 13.96))
-
     
-    while not (DoesCutsceneEntityExist('MP_1', 0)) do
-        Wait(0)
+    return true
+end
+
+function takeOffLSIA()
+    playCutsceneTravel(CayoFlightConfig.LSIA_TO_CAYO)
+end
+
+function takeOffCayo()
+    playCutsceneTravel(CayoFlightConfig.CAYO_TO_LSIA)
+end
+
+AddEventHandler('esx:onPlayerLoaded', function(playerData)
+    for k,v in pairs(CayoFlightConfig) do
+        if v.target then
+            local pedModel = joaat(v.target.ped)
+            lib.requestModel(pedModel)
+
+            spawnedPeds[k] = CreatePed(4, pedModel, v.target.coords.x, v.target.coords.y, v.target.coords.z, v.target.coords.w, false, true)
+            SetEntityInvincible(spawnedPeds[k], true)
+            SetBlockingOfNonTemporaryEvents(spawnedPeds[k], true)
+            FreezeEntityPosition(spawnedPeds[k], true)
+            ox_target:addLocalEntity(spawnedPeds[k], v.target.options)
+        end
+    end
+end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
     end
 
-    ClonePedToTarget(plyrId, clone)
+    for k,v in pairs(CayoFlightConfig) do
+        if v.target then
+            local pedModel = joaat(v.target.ped)
+            lib.requestModel(pedModel)
 
-    Wait(10000)
-    DoScreenFadeOut(500)
-    Wait(2000)
-    StopCutsceneImmediately()
-    DoScreenFadeIn(500)    
-end) 
+            spawnedPeds[k] = CreatePed(4, pedModel, v.target.coords.x, v.target.coords.y, v.target.coords.z, v.target.coords.w, false, true)
+            SetEntityInvincible(spawnedPeds[k], true)
+            SetBlockingOfNonTemporaryEvents(spawnedPeds[k], true)
+            FreezeEntityPosition(spawnedPeds[k], true)
+            ox_target:addLocalEntity(spawnedPeds[k], v.target.options)
+        end
+    end
+end)
 
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
 
+    for k,v in pairs(spawnedPeds) do
+        if DoesEntityExist(v) then
+            ox_target:removeLocalEntity(v)
+            DeletePed(v)
+        end
+    end
+end)
